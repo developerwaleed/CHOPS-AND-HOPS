54e8e74daf39facbfc23eaed286f209ddff2e761
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-31 08:47:24 +0500

    Make navbar Responsive

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import './style.css';
 import fetchItems from './js/home/fetchitems.js';
 import logo from './assets/logo.png';
 
-const logoAttr = document.getElementById('logo');
-logoAttr.src = logo;
+const logoAttrDesk = document.getElementById('logo');
+const logoAttrMob = document.getElementById('logoMob');
+const closeNav = document.getElementById('closeNav');
+const mySidenav = document.getElementById('mySidenav');
+const main = document.getElementById('main');
+
+logoAttrDesk.src = logo;
+logoAttrMob.src = logo;
+
+logoAttrMob.addEventListener('click', () => {
+  mySidenav.style.width = '250px';
+  main.style.marginLeft = '250px';
+  logoAttrMob.style.marginLeft = '250px';
+});
+
+closeNav.addEventListener('click', () => {
+  mySidenav.style.width = '0';
+  main.style.marginLeft = '0';
+  logoAttrMob.style.marginLeft = '0';
+});
+
 window.onload = fetchItems();
